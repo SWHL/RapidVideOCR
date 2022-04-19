9f675b6b03f0a9d724918a5918d31563eefbb324
SWHL <liekkaskono@163.com>
2022-04-19 18:11:56 +0800

    Add ASR

--- tmp.py
+++ tmp.py
@@ -8,12 +8,12 @@ from pydub import AudioSegment
 # os.putenv('PATH', r'E:\Program Files\ffmpeg-n5.0-latest-win64-lgpl-shared-5.0\bin')
 
 mp4_path = 'assets/test_video/2.mp4'
-audio = AudioSegment.from_file(mp4_path, 'mp4')
+audio = AudioSegment.from_file(mp4_path, 'mp4').set_frame_rate(16000)
 
 # 毫秒
-start = 458
-end = 1166
+start = 1583
+end = 2541
 
 part = audio[start:end]
 
-part.export('1.mp3', format='mp3')
+part.export('1.wav', format='wav')
