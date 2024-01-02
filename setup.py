cfcd765f24f935269c24cca5dcf25beadfd0ff36
SWHL <liekkaskono@163.com>
2024-01-02 10:31:08 +0800

    Update to v2.2.10

--- setup.py
+++ setup.py
@@ -13,28 +13,29 @@ def read_txt(txt_path: str) -> List:
     if not isinstance(txt_path, str):
         txt_path = str(txt_path)
 
-    with open(txt_path, 'r', encoding='utf-8') as f:
-        data = list(map(lambda x: x.rstrip('\n'), f))
+    with open(txt_path, "r", encoding="utf-8") as f:
+        data = list(map(lambda x: x.rstrip("\n"), f))
     return data
 
 
 def get_readme() -> str:
     root_dir = Path(__file__).resolve().parent
-    readme_path = str(root_dir / 'docs' / 'doc_whl.md')
-    with open(readme_path, 'r', encoding='utf-8') as f:
+    readme_path = str(root_dir / "docs" / "doc_whl.md")
+    with open(readme_path, "r", encoding="utf-8") as f:
         readme = f.read()
     return readme
 
 
-MODULE_NAME = 'rapid_videocr'
+MODULE_NAME = "rapid_videocr"
 
 obtainer = GetPyPiLatestVersion()
 latest_version = obtainer(MODULE_NAME)
 VERSION_NUM = obtainer.version_add_one(latest_version)
+VERSION_NUM = obtainer.version_add_one(VERSION_NUM)
 
 # 优先提取commit message中的语义化版本号，如无，则自动加1
 if len(sys.argv) > 2:
-    match_str = ' '.join(sys.argv[2:])
+    match_str = " ".join(sys.argv[2:])
     matched_versions = obtainer.extract_version(match_str)
     if matched_versions:
         VERSION_NUM = matched_versions
@@ -46,26 +47,26 @@ setuptools.setup(
     platforms="Any",
     description="Tool for extracting hard subtitles from videos.",
     long_description=get_readme(),
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="SWHL",
     author_email="liekkaskono@163.com",
     url="https://github.com/SWHL/RapidVideOCR.git",
-    license='Apache-2.0',
+    license="Apache-2.0",
     include_package_data=True,
-    install_requires=read_txt('requirements.txt'),
+    install_requires=read_txt("requirements.txt"),
     packages=[MODULE_NAME],
-    package_data={'': ['*.yaml']},
-    keywords=['rapidocr,videocr,subtitle'],
+    package_data={"": ["*.yaml"]},
+    keywords=["rapidocr,videocr,subtitle"],
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
-    python_requires='>=3.6,<3.12',
+    python_requires=">=3.6,<3.12",
     entry_points={
-        'console_scripts': [f'{MODULE_NAME}={MODULE_NAME}.main:main'],
+        "console_scripts": [f"{MODULE_NAME}={MODULE_NAME}.main:main"],
     },
 )
