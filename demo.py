1eb9981f88a83eb538547598910f6b5aadcf529a
SWHL <liekkaskono@163.com>
2025-04-25 08:00:07 +0800

    chore: update demo

--- demo.py
+++ demo.py
@@ -9,9 +9,7 @@ vsf_exe_path = (
     r"G:\ProgramFiles\VideoSubFinder_6.10_x64\Release_x64\VideoSubFinderWXW.exe"
 )
 vsf_input_params = VideoSubFinderInput(vsf_exe_path=vsf_exe_path)
-ocr_input_params = RapidVideOCRInput(
-    is_batch_rec=False, ocr_params={"Global.with_paddle": True}
-)
+ocr_input_params = RapidVideOCRInput(is_batch_rec=False)
 vsf_ocr = RapidVideoSubFinderOCR(vsf_input_params, ocr_input_params)
 
 # video_path可以是目录或者具体video路径
@@ -23,9 +21,7 @@ vsf_ocr(video_path, save_dir)
 # # 只识别
 from rapid_videocr import RapidVideOCR, RapidVideOCRInput
 
-ocr_input_params = RapidVideOCRInput(
-    is_batch_rec=False, ocr_params={"Global.with_paddle": True}
-)
+ocr_input_params = RapidVideOCRInput(is_batch_rec=False)
 extractor = RapidVideOCR(ocr_input_params)
 
 rgb_dir = "tests/test_files/RGBImages"
